d1a407d262a1335d94776031ee290c4bbcc112e3
minemaster <glebbasmanov123@gmail.com>
2024-02-18 14:59:43 +0400

    Теперь точно Initial commit

---
+++ core/src/com/gdt/spacefactory/Main.java
@@ -0,0 +1,94 @@
+package com.gdt.spacefactory;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Vector;
+
+
+public class Main extends Game {
+	public static String debugData;
+	public static ArrayList<String> messages = new ArrayList<>();
+	public Screen scene_main;
+	public Screen Scene_menu;
+	public Screen scene_startMain;
+
+	//    Vector2 pos;
+	@Override
+	public void create() {
+//        if (Gdx.app.getType()== Application.ApplicationType.Desktop){
+//
+//        }
+		data.init();
+//        scene_main=new Scene_main();
+		Scene_menu = new Scene_menu();
+//        System.out.println("Main scene created");
+		setScreen(Scene_menu);
+//        System.out.println(pos.x*10);
+	}
+
+
+	@Override
+	public void render() {
+		if (messages.size() > 0) {
+			for (int i = 0; i < messages.size(); i++) {
+//				if (messages.get(i).equals("open scene_startMain")) {
+//					scene_startMain = new Scene_startMain();
+//					setScreen(scene_startMain);
+//					scene_startMain.show();
+//					messages.remove(i);
+//					break;
+//				}
+//				if (messages.get(i).equals("open scene_main")) {
+//					scene_main = new Scene_main();
+//					setScreen(scene_main);
+//					scene_main.show();
+//					messages.remove(i);
+//					break;
+//				}
+			}
+		}
+		getScreen().render(Gdx.graphics.getDeltaTime());
+		debugData = String.valueOf(Gdx.graphics.getFramesPerSecond());
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		getScreen().resize(width, height);
+	}
+
+	@Override
+	public void dispose() {
+		getScreen().dispose();
+	}
+
+
+//    public static Vector2 randScreenPoint(Camera camera){
+//
+//    }
+}
